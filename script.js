6be5dbe80a8025ef0d9f8ec9026c798ef126cd37
Mohammad Haris <112751385+mdharis98@users.noreply.github.com>
2024-10-01 22:18:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your code here
+let draggedElement = null;
+
+document.querySelectorAll('.image').forEach(div => {
+    // When drag starts
+    div.addEventListener('dragstart', function(e) {
+        draggedElement = e.target;  
+        e.dataTransfer.setData('text/plain', null);  
+    });
+
+    div.addEventListener('dragover', function(e) {
+        e.preventDefault();  
+    });
+
+    div.addEventListener('drop', function(e) {
+        e.preventDefault();
+        
+       
+        if (draggedElement !== e.target) {
+            const draggedImage = window.getComputedStyle(draggedElement).backgroundImage;
+            const targetImage = window.getComputedStyle(e.target).backgroundImage;
+            
+            draggedElement.style.backgroundImage = targetImage;
+            e.target.style.backgroundImage = draggedImage;
+        }
+    });
+});
